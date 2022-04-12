9c7d2b080f11016d7ad85cfffb3682a4ce60806a
Brendan Potter <brendanpotter00@gmail.com>
2022-04-11 22:54:22 -0500

    dirty deployment

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { loginUrl } from "./spotify";
+import { deployedLoginUrl } from "./spotify";
 import "./login.css";
 
 function Login() {
@@ -7,7 +8,10 @@ function Login() {
     <div className="login">
       I am the login button
       <button className="spotify-button">
-        <a href={loginUrl}>Login</a>
+        <a href={loginUrl}>Login for testing</a>
+      </button>
+      <button className="spotify-button">
+        <a href={deployedLoginUrl}>Login for deployment</a>
       </button>
     </div>
   );
