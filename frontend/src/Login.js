aa94e4333fab94fd8bd3c832ffc13af74b574c24
Brendan Potter <brendanpotter00@gmail.com>
2022-04-11 23:22:52 -0500

    optional button function

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,15 +4,19 @@ import { deployedLoginUrl } from "./spotify";
 import "./login.css";
 
 function Login() {
+  console.log(window.location.href);
   return (
     <div className="login">
       I am the login button
-      <button className="spotify-button">
-        <a href={loginUrl}>Login for testing</a>
-      </button>
-      <button className="spotify-button">
-        <a href={deployedLoginUrl}>Login for deployment</a>
-      </button>
+      {window.location.href == "http://localhost:3000/Spotify_Uncovered_v2" ? (
+        <button className="spotify-button">
+          <a href={loginUrl}>Login for testing</a>
+        </button>
+      ) : (
+        <button className="spotify-button">
+          <a href={deployedLoginUrl}>Login for deployment</a>
+        </button>
+      )}
     </div>
   );
 }
