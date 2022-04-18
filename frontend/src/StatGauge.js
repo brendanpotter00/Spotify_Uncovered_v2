3a07749841b025405182be400f6ce99c4d2fd750
ldang1 <linhtdm2001@gmail.com>
2022-04-17 21:41:50 -0500

    add stats bar

--- frontend/src/StatGauge.js
+++ frontend/src/StatGauge.js
@@ -1,7 +1,63 @@
 import React from "react";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { buildStyles } from 'react-circular-progressbar';
+import "./statGauge.css";
+import 'react-circular-progressbar/dist/styles.css';
 
+const percentage = 66;
 function StatGauge({ props }) {
-  return <div>StatGauge</div>;
+  return (
+    <div className="bar">
+      <div className="firstbar">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            rotation: 0.25,
+            strokeLinecap: 'round',
+            textSize: '16px',
+            pathTransitionDuration: 0.5,
+            pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+            textColor: '#f88',
+            trailColor: '#d6d6d6',
+            backgroundColor: '#3e98c7',
+            })}
+        />
+      </div>
+      <div className="secondbar">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            rotation: 0.25,
+            strokeLinecap: 'round',
+            textSize: '16px',
+            pathTransitionDuration: 0.5,
+            pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+            textColor: '#f88',
+            trailColor: '#d6d6d6',
+            backgroundColor: '#3e98c7',
+            })}
+        />
+      </div>
+      <div className="thirdbar">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            rotation: 0.25,
+            strokeLinecap: 'round',
+            textSize: '16px',
+            pathTransitionDuration: 0.5,
+            pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+            textColor: '#f88',
+            trailColor: '#d6d6d6',
+            backgroundColor: '#3e98c7',
+            })}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default StatGauge;
