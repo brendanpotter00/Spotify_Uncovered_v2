c9ea7c3e56f8bf3af7a1bffde77b0573e5eb43ed
bpotter-trinity <brendanpotter00@gmail.com>
2022-04-20 00:10:14 -0500

    mapped the avg stats to the bars

--- frontend/src/StatGauge.js
+++ frontend/src/StatGauge.js
@@ -6,34 +6,54 @@ import "react-circular-progressbar/dist/styles.css";
 
 const percentage = 66;
 function StatGauge({ props }) {
-  const firstSongValence = 0;
   console.log(props);
   //props[0].valence;
+  //console.log(props[0].valence);
+
+  function avgForMetrics(metrics) {
+    return Math.round(metrics.reduce((a, b) => a + b, 0) / metrics.length);
+  }
+
+  //getting all energies, loudnesses, and valences in arrays
+  let allEnergies = [];
+  let allValences = [];
+  let allLoudnesses = [];
+  for (let track of props) {
+    allEnergies.push(track.energy);
+    allValences.push(track.valence);
+    allLoudnesses.push(track.loudness);
+  }
+
+  //getting avgerages of all metrics to place in the bars
+  let energyAvg = avgForMetrics(allEnergies);
+  let valenceAvg = avgForMetrics(allValences);
+  let loudnessAvg = avgForMetrics(allLoudnesses);
+
   return (
     <div className="bars">
       <div className="firstbar">
         <CircularProgressbar
           className="circleBar"
-          value={percentage}
-          text={`${firstSongValence}%`}
+          value={energyAvg * 10}
+          text={`${energyAvg}`}
           styles={buildStyles({
             rotation: 0.25,
             strokeLinecap: "round",
             textSize: "16px",
             pathTransitionDuration: 0.5,
-            pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+            pathColor: `rgba(62, 152, 199, ${percentage / 10})`,
             textColor: "#f88",
             trailColor: "#d6d6d6",
             backgroundColor: "#3e98c7",
           })}
         />
-        <h3> Bar 1 </h3>
+        <h3> Energy </h3>
       </div>
       <div className="secondbar">
         <CircularProgressbar
           className="circleBar"
-          value={percentage}
-          text={`${percentage}%`}
+          value={valenceAvg * 10}
+          text={`${valenceAvg}`}
           styles={buildStyles({
             rotation: 0.25,
             strokeLinecap: "round",
@@ -45,13 +65,13 @@ function StatGauge({ props }) {
             backgroundColor: "#3e98c7",
           })}
         />
-        <h3> Bar 2 </h3>
+        <h3> Valence </h3>
       </div>
       <div className="thirdbar">
         <CircularProgressbar
           className="circleBar"
-          value={percentage}
-          text={`${percentage}%`}
+          value={loudnessAvg * 10}
+          text={`${loudnessAvg}`}
           styles={buildStyles({
             rotation: 0.25,
             strokeLinecap: "round",
@@ -63,7 +83,7 @@ function StatGauge({ props }) {
             backgroundColor: "#3e98c7",
           })}
         />
-        <h3> Bar 3 </h3>
+        <h3> Loudness </h3>
       </div>
     </div>
   );
