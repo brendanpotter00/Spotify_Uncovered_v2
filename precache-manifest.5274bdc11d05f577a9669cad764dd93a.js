f63dbfbae5334ae2b8dd44be0b0b200857216984
bpotter-trinity <brendanpotter00@gmail.com>
2022-04-22 00:35:01 -0500

    Updates

---
+++ precache-manifest.5274bdc11d05f577a9669cad764dd93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4526ff27c97256a9cfb",
+    "url": "/Spotify_Uncovered_v2/static/css/main.853b996a.chunk.css"
+  },
+  {
+    "revision": "e4526ff27c97256a9cfb",
+    "url": "/Spotify_Uncovered_v2/static/js/main.e4526ff2.chunk.js"
+  },
+  {
+    "revision": "9720b17ce44f7d5cf224",
+    "url": "/Spotify_Uncovered_v2/static/css/1.23479bba.chunk.css"
+  },
+  {
+    "revision": "9720b17ce44f7d5cf224",
+    "url": "/Spotify_Uncovered_v2/static/js/1.9720b17c.chunk.js"
+  },
+  {
+    "revision": "f3307719dd85bc1da9b2",
+    "url": "/Spotify_Uncovered_v2/static/js/runtime~main.f3307719.js"
+  },
+  {
+    "revision": "343909e91338606a37c59bc0d1dea6a6",
+    "url": "/Spotify_Uncovered_v2/index.html"
+  }
+];
